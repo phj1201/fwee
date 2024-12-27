6685c24c38ebe14d2aa10afdcda12704df17066c
Park Hyeonjin <phyeonjin00@gmail.com>
2024-12-27 10:46:19 +0900

    js edited
    js

--- contents/js/brand-identity.js
+++ contents/js/brand-identity.js
@@ -1,8 +1,26 @@
 $(function (){
     
     $('.card-box').mouseover(function (){
-        $('.card-box img').attr('src', '../contents/img/brand/brandid_con1_aft.jpg').show().fadeIn();
+        $('.card-box img').attr('src', '../contents/img/brand/brandid_con1_aft.jpg');
     }).mouseout(function (){
-        $('.card-box img').attr('src', '../contents/img/brand/brandid_con1_bfe.jpg').fadeOut()
+        $('.card-box img').attr('src', '../contents/img/brand/brandid_con1_bfe.jpg');
+    });
+
+    $('.cap1').mouseover(function (){
+        $('.cap1 .img1').show().fadeIn()
+    }).mouseout(function (){
+        $('.cap1 .img1').hide().fadeOut()
+    });
+
+    $('.cap2').mouseover(function (){
+        $('.cap2 .img2').show().fadeIn()
+    }).mouseout(function (){
+        $('.cap2 .img2').hide().fadeOut()
+    });
+
+    $('.cap3').mouseover(function (){
+        $('.cap3 .img3').show().fadeIn()
+    }).mouseout(function (){
+        $('.cap3 .img3').hide().fadeOut()
     });
 });
