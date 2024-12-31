a10b821fb6fad3de4bc21064ac75ea68bda01f48
Park Hyeonjin <phyeonjin00@gmail.com>
2024-12-31 15:54:41 +0900

    edited

--- contents/js/store.js
+++ contents/js/store.js
@@ -10,18 +10,47 @@
 
 */
 
-    $(document).ready(function () {
+$(document).ready(function () {
 
-        $('.card').find('.img-box').animate({
-            width:'50%'
-        }, 800, function (){
-            $('.card').find('.des').fadeIn(600);
+    var Rbox = $('.right-box');
+    var Lbox = $('.left-box');
+
+    Rbox.find('.img-box').animate({
+       width: '640px'
+    }, 800, function () {
+        Rbox.find('.des').fadeIn(600);
+    }); 
+
+    Lbox.find('.img-box').animate({
+        //width: '50%'
+       width: '640px'
+    }, 800, function () {
+        Lbox.find('.des').fadeIn(600);
+    }); 
+
+});
+
+$(window).scroll(function () {
+
+    var scTop = $(this).scrollTop();
+    console.log(scTop);
+
+    var winHeight = $(this).height();
+    console.log('브라우저 화면의 높이: ' + winHeight);
+
+    var gap = Math.ceil(winHeight * 0.9);
+    console.log('기준간격: ' + gap);
+
+    //3번 카드
+    var box3pos = $('.last').offset().top - gap;
+    console.log('3번 박스 등장 기준값: ' + box3pos);
+
+    if (scTop > box3pos) {
+        $('.last').find('.img-box').animate({
+            width: '640px'
+        }, 800, function () {
+            $(this).next().fadeIn(600);
         });
+    }
 
-        
-
-
-        //스크롤시 순서대로 나타나기
-
-    });
-    
+});
